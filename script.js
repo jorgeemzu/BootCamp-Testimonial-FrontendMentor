a998fc3d1b1a9bd14698ae5f302c9206dfa53509
jorgeemzu <jorge.emzu@gmail.com>
2020-06-19 11:17:57 -0430

    modified:   index.html 	modified:   script.js 	modified:   styles.css
    Added slide functions on script.js

--- script.js
+++ script.js
@@ -4,6 +4,56 @@ let slideImg = document.querySelectorAll('.slaid__img img'),
     slideTextProf = document.querySelectorAll('.text__testimonial--info p'),
     slide = document.querySelectorAll('.slaid'),
     arrowLeft = document.querySelector('.btn__left'),
-    arrowRigth = document.querySelector('.btn__rigth');
+    arrowRigth = document.querySelector('.btn__rigth'),
+    current = 0;
 
 
+        function reset(){
+            for(let i of slide){
+                i.style.display = 'none'
+            }
+        }
+
+        function startSlide(){
+            reset()
+
+            slide[0].style.display = 'block'
+        }
+
+        function goLeft(){
+            reset();
+
+            slide[current - 1].style.display = 'block'
+            
+            console.log(slide[current]);
+            current--;
+        }
+
+        function goRight(){
+            reset();
+            slide[current + 1].style.display = 'block'
+            
+            current ++;
+        }
+
+        arrowRigth.addEventListener('click', () =>{
+            
+            if (current === slide.length - 1){
+                current = -1;
+            }
+
+            goRight();
+        })
+
+        arrowLeft.addEventListener('click', () =>{
+            if (current === 0){
+                current = slide.length;
+                
+            }
+
+            goLeft();
+        })
+
+        startSlide();
+
+       
