6a22db953b24a1168094e403c92ccd1a4bca6e68
jorgeemzu <jorge.emzu@gmail.com>
2020-06-18 23:44:14 -0430

    new file:   script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+let slideImg = document.querySelectorAll('.slaid__img img'),
+    slideTextTest = document.querySelectorAll('.text__testimonial p'),
+    slideTextName = document.querySelectorAll('.text__testimonial--info h2'),
+    slideTextProf = document.querySelectorAll('.text__testimonial--info p'),
+    slide = document.querySelectorAll('.slaid'),
+    arrowLeft = document.querySelector('.btn__left'),
+    arrowRigth = document.querySelector('.btn__rigth');
+
+
